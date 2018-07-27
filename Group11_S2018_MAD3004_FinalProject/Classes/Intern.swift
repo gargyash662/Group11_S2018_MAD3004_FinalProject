b7b3b934d8ae84185516cd13b54ec245b3b55bda
yash <gargyash662@gmail.com>
2018-07-26 20:54:24 -0400

    commit

---
+++ Group11_S2018_MAD3004_FinalProject/Classes/Intern.swift
@@ -0,0 +1,26 @@
+//
+//  Intern.swift
+//  Group11_S2018_MAD3004_FinalProject
+//
+//  Created by yash on 26/07/18.
+//  Copyright © 2018 yash. All rights reserved.
+//
+
+import Foundation
+
+
+class intern: employee {
+    var schoolname: String!
+    
+    init(schoolname: String, Name: String, birthday: String)
+    {
+        super.init(Name: Name, birthday: birthday)
+        self.schoolname = schoolname
+       
+        
+    }
+    
+  override  func  display() {
+        print("Inter details \(schoolname!) \(Name!) \(birthday!)")
+    }
+}

e9b6e74a9c09dbd5016ee3cd8a41c522b9a5896b
yash <gargyash662@gmail.com>
2018-07-30 18:03:45 -0400

    gramatical changes

--- Group11_S2018_MAD3004_FinalProject/main.swift
+++ Group11_S2018_MAD3004_FinalProject/main.swift
@@ -8,8 +8,7 @@
 
 import Foundation
 
-
-
+print("----------Group 11------------ ")
 var v1 = car(make: "Civic", plate: "cdef345", companyname: "Honda", year: 2016, colour: "Black")
 var v2 = car(make: "Suv", plate: "fdhk215", companyname: "Kia", year: 2015, colour: "Blue")
 var v3 = motorcycle(make: "Yamaha", plate: "Mh30", cc: 180, milaege: 40)
@@ -18,28 +17,28 @@ var v4 = car(make: "Duster", plate: "Cg07", companyname: "Renault", year: 2018, 
 
 
 
-var e7 = intern(schoolname: "lambton colege", Name: "john day", birthday: "12/07/1998", vehicle: [v5])
-e7.display()
+var e1 = intern(schoolname: "lambton colege", Name: "john day", birthday: "12/07/1998", vehicle: [v5])
+e1.display()
 let line = String(repeating: "*", count: 50)
 print(line)
 
-var e6 = fulltime(salary: 10000.0, bonus: 100.0, Name: "abc", birthday: "12/03/1995", vehicle: [v1])
-e6.display()
+var e2 = fulltime(salary: 10000.0, bonus: 100.0, Name: "abc", birthday: "12/03/1995", vehicle: [v1])
+e2.display()
 //let line = String(repeating: "*", count: 50)
 print(line)
 
 
-var e8 = parttime(Name: "yash garg", birthday: "03/13/1995", rate: 14, hours: 8)
-e8.display()
+var e3 = parttime(Name: "yash garg", birthday: "03/13/1995", rate: 14, hours: 8)
+e3.display()
 //let line = String(repeating: "*", count: 50)
 print(line)
 
-var e9 = Fixedbased(Name: "Ajay", birthday: "05/29/1969", rate: 23, fixedamount: 300, hoursworked: 8, vehicle: [v4])
-e9.display()
+var e4 = Fixedbased(Name: "Ajay", birthday: "05/29/1969", rate: 23, fixedamount: 300, hoursworked: 8, vehicle: [v4])
+e4.display()
 print(line)
 
-var e10 = commisionedbase(commissionperc: 3.0, commisionrate: 3.6, Name: "Rajan", birthday: "12/05/1989", rate: 80, hours: 7.5, vehicle: [v2])
-e10.display()
+var e5 = commisionedbase(commissionperc: 3.0, commisionrate: 3.6, Name: "Rajan", birthday: "12/05/1989", rate: 80, hours: 7.5, vehicle: [v2])
+e5.display()
 print(line)
 
 
